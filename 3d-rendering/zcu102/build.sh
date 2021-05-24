b7744b007c45596825cc2c2dd8cb81c9e8391f78
Yuanlong Xiao <yuanlongxiao24@gmail.com>
2021-05-23 21:58:58 -0400

    part 1

---
+++ 3d-rendering/zcu102/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cd ../../
+root_dir=$(pwd)
+cd -
+
+
+export PLATFORM_REPO_PATHS=${root_dir}/embedded_platform/zcu102_custom
+export ROOTFS=${root_dir}/embedded_platform/linux
+export kl_name=rendering
+export MaxJobNum=$(nproc)
+source ${root_dir}/embedded_platform/sdkdir/environment-setup-aarch64-xilinx-linux
+source /opt/Xilinx/Vitis/2020.2/settings64.sh
+
+# Make sure everything is up to date
+make all 
+
+
+
+# Exit when any command fails
+#set -e
+#if [[ -z "$ROOTFS" ]]; then
+#   echo "Error: make sure to set the ROOTFS environment variable"
+#   exit
+#fi
+#if [[ -z "$SYSROOT" ]]; then
+#   echo "Error: make sure to set the SYSROOT environment variable"
+#   exit
+#fi
+#if [[ -z "$PLATFORM_REPO_PATHS" ]]; then
+#   echo "Error: make sure to set the PLATFORM_REPO_PATHS environment variable"
+#   exit
+#fi
+
+
