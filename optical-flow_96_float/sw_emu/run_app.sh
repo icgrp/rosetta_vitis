659374fbec48a9a3ea5598032ec83a1a357f9db7
dj-park <sologoal@naver.com>
2022-10-16 18:01:05 -0400

    optical flow, hw verified

---
+++ optical-flow_96_float/sw_emu/run_app.sh
@@ -0,0 +1,8 @@
+#
+# In QEMU, once Linux has booted, run this script by entering the following command:
+#
+
+export XCL_EMULATION_MODE=sw_emu
+./app.exe optical_flow.xclbin
+
+echo "INFO: press Ctrl+a x to exit qemu"
