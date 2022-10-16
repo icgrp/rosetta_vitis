f6ac4436a7f7e9701b675bc8ea88dce248961f10
dj-park <sologoal@naver.com>
2022-10-16 18:14:44 -0400

    spam, hw verified

---
+++ spam-filter_par_64/sw_emu/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# added by dopark
+export PLATFORM_REPO_PATHS=/opt/platforms/xilinx_zcu102_base_202110_1
+export ROOTFS=/opt/platforms/xilinx-zynqmp-common-v2021.1
+#export LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
+unset LD_LIBRARY_PATH
+source /opt/platforms/xilinx-zynqmp-common-v2021.1/ir/environment-setup-cortexa72-cortexa53-xilinx-linux
+export kl_name=optical_flow
+export MaxJobNum=$(nproc)
+
+
+# Make sure everything is up to date
+make all 
