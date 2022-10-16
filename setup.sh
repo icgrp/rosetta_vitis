1cb4af45b188e89cabc48cf7172383b84e5dea6a
dj-park <sologoal@naver.com>
2022-10-16 16:46:46 -0400

    rendering sw_emu, hw verified

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
- cd platform/
- cat ./embedded_platform.tar.gz.?? > embedded_platform.tar.gz
- tar -zxvf ./embedded_platform.tar.gz 
- cd embedded_platform/
- mkdir sdkdir
- ./linux/sdk.sh -y -d sdkdir
