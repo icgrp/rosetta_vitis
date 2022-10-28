7366de3d165fbfdd874313176c1816b3a8432a46
dj-park <sologoal@naver.com>
2022-10-28 10:35:54 -0400

    embedded platform no need xrt

---
+++ run_all.sh
@@ -0,0 +1,21 @@
+cd 3d-rendering/zcu102/
+./build.sh
+cd ../..
+cd digit-recognition_par_40/zcu102/
+./build.sh
+cd ../..
+cd digit-recognition_par_80/zcu102/
+./build.sh
+cd ../..
+cd optical-flow_64/zcu102/
+./build.sh
+cd ../..
+cd optical-flow_96_float/zcu102/
+./build.sh
+cd ../..
+cd spam-filter_par_32/zcu102/
+./build.sh
+cd ../..
+cd spam-filter_par_64/zcu102/
+./build.sh
+cd ../..
